d43ccbc6290b440b7223ea1b4da9d7e7e562d6df
nahidsaikat <nahidsaikatft40@gmail.com>
2020-05-30 02:56:39 +0600

    Add create_app factory function

---
+++ application/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+# Globally accessible libraries
+db = SQLAlchemy()
+
+def create_app():
+    """Initialize the core application."""
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object('config.Config')
+
+    # Initialize Plugins
+    db.init_app(app)
+
+    with app.app_context():
+        # Register Blueprints
+
+        return app
