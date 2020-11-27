87a334fcfc0e32e833f4d24af2eb83924f3b9a4a
nahidsaikat <nahidsaikatft40@gmail.com>
2020-11-27 11:44:29 +0600

    Configuring flask blueprint and flask asset library

--- application/__init__.py
+++ application/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CsrfProtect
+from flask_assets import Environment
 
 
 # Globally accessible libraries
 csrf = CsrfProtect()
 db = SQLAlchemy()
+f_assets = Environment()
 
 
 def create_app():
@@ -20,13 +22,19 @@ def create_app():
         app.config.from_object('config.TestConfig')
     else:
         app.config.from_object('config.DevConfig')
+    print(f'The ENV is set to {app.config["ENV"]}')
 
     # Initialize Plugins
     csrf.init_app(app)
     db.init_app(app)
+    f_assets.init_app(app)
 
     with app.app_context():
-        from .core import routes
-        # Register Blueprints
+        from application.core.views import core_bp
+        from application.assets import compile_static_assets
 
+        # Register Blueprints
+        app.register_blueprint(core_bp)
+
+        compile_static_assets(f_assets)
         return app
