586262e047a77e9f4e915ed7c6ef55b1a74fffdd
nahidsaikat <nahidsaikatft40@gmail.com>
2021-03-25 06:56:27 +0600

    Formet user model and add .flaskenv file

--- easy_food/core/models.py
+++ easy_food/core/models.py
@@ -9,44 +9,13 @@ class User(db.Model):
 
     __tablename__ = 'users'
 
-    id = db.Column(
-        db.Integer,
-        primary_key=True
-    )
-    name = db.Column(
-        db.String(100),
-        nullable=False,
-        unique=False
-    )
-    email = db.Column(
-        db.String(40),
-        unique=True,
-        nullable=False
-    )
-    password = db.Column(
-        db.String(200),
-        primary_key=False,
-        unique=False,
-        nullable=False
-    )
-    website = db.Column(
-        db.String(60),
-        index=False,
-        unique=False,
-        nullable=True
-    )
-    created_on = db.Column(
-        db.DateTime,
-        index=False,
-        unique=False,
-        nullable=True
-    )
-    last_login = db.Column(
-        db.DateTime,
-        index=False,
-        unique=False,
-        nullable=True
-    )
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=False)
+    email = db.Column(db.String(40), unique=True, nullable=False)
+    password = db.Column(db.String(200), primary_key=False, unique=False, nullable=False)
+    website = db.Column(db.String(60), index=False, unique=False, nullable=True)
+    created_on = db.Column(db.DateTime, index=False, unique=False, nullable=True)
+    last_login = db.Column(db.DateTime, index=False, unique=False, nullable=True)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
